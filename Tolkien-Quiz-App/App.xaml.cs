1df0bb4b539c724f28f6b1b7729d34b6efd93627
mart-gant <martgant@gmail.com>
2024-11-01 18:08:12 +0100

    Dodaj pliki projektów.

---
+++ Tolkien-Quiz-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tolkien_Quiz_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Tolkien-Quiz-App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tolkien_Quiz_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
